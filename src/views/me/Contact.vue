07ef952a79902482ee42ad7f8c229ef44dfc2ef0
style|yong <style|yong@yong.com>
2019-06-26 23:05:46 +0800

    init

--- src/views/me/Contact.vue
+++ src/views/me/Contact.vue
@@ -8,25 +8,33 @@
             <p>请勿把广告与无相关学习与共同交流的超链接带进来。</p>
             <ul class="qq_join">
                 <li>
-                    <section><a href="https://jq.qq.com/?_wv=1027&amp;k=5EH6xZg"><img src="https://www.yangqq.com/skin/jxhx/images/qq1.png" /> </a>
-                        <p><a href="https://jq.qq.com/?_wv=1027&amp;k=5EH6xZg">网页设计交流吧①</a></p><a href="https://jq.qq.com/?_wv=1027&amp;k=5EH6xZg"> </a>
-                        <p><a href="https://jq.qq.com/?_wv=1027&amp;k=5EH6xZg">QQ群号：280998807</a></p><a href="https://jq.qq.com/?_wv=1027&amp;k=5EH6xZg"> </a></section></li>
+                    <section>
+                        <p><a href="">网页设计交流吧①</a></p><a href=""> </a>
+                        <p><a href="">QQ群号：xxxxxxxxx</a></p><a href=""> </a>
+                    </section>
+                </li>
                 <li>
-                    <section><a href="https://jq.qq.com/?_wv=1027&amp;k=52fM36y"><img src="https://www.yangqq.com/skin/jxhx/images/qq2.png" /> </a>
-                        <p><a href="https://jq.qq.com/?_wv=1027&amp;k=52fM36y">网页设计交流吧②</a></p><a href="https://jq.qq.com/?_wv=1027&amp;k=52fM36y"> </a>
-                        <p><a href="https://jq.qq.com/?_wv=1027&amp;k=52fM36y">QQ群号：291195645</a></p><a href="https://jq.qq.com/?_wv=1027&amp;k=52fM36y"> </a></section></li>
+                    <section>
+                        <p><a href="">网页设计交流吧①</a></p><a href=""> </a>
+                        <p><a href="">QQ群号：xxxxxxxxx</a></p><a href=""> </a>
+                    </section>
+                </li>
             </ul>
             <h2>微信群/微信公众号</h2>
-            <p>微信群①已满，现在可以加入群②，加微信号476847113备注&ldquo;进群&rdquo;</p>
+            <p>微信群①已满，现在可以加入群②，加微信号xxxxx备注&ldquo;进群&rdquo;</p>
             <ul class="qq_join">
                 <li>
-                    <section><img src="https://www.yangqq.com/skin/jxhx/images/joinwxqun.png" />
-                        <p>网页设计交流<b>微信群</b></p>
-                        <p>加476847113备注&ldquo;进群&rdquo;</p></section></li>
+                    <section>
+                        <p><a href="">网页设计交流吧①</a></p><a href=""> </a>
+                        <p><a href="">QQ群号：xxxxxxxxx</a></p><a href=""> </a>
+                    </section>
+                </li>
                 <li>
-                    <section><img src="https://www.yangqq.com/skin/jxhx/images/wxgzh.jpg" />
-                        <p>扫描关注青于<b>蓝微信公众号</b></p>
-                        <p>第一时间获取最新模板信息</p></section></li>
+                    <section>
+                        <p><a href="">网页设计交流吧①</a></p><a href=""> </a>
+                        <p><a href="">QQ群号：xxxxxxxxx</a></p><a href=""> </a>
+                    </section>
+                </li>
             </ul>
         </div>
     </div>
